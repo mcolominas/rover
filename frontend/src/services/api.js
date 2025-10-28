620b74c109ac17f3698d7b993e7f2ef7ffa80ccb
Marc <mcolominasrojas@gmail.com>
2025-10-28 13:22:30 +0100

    Handle API downtime with friendly error messages

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,12 +2,24 @@ import axios from 'axios'
 
 const API_URL = 'http://localhost:8000/api'
 
+function getErrorMessage(error) {
+  if (error.response && error.response.data) {
+    return error.response.data
+  }
+
+  if (error.request) {
+    return { message: "Unable to connect to the planetary server. Please try again later." }
+  }
+
+  return { message: "An unexpected error occurred. Please try again." }
+}
+
 export const createPlanet = async () => {
   try {
     const response = await axios.post(`${API_URL}/planet`)
     return response.data
   } catch (error) {
-    throw error.response.data
+    throw getErrorMessage(error)
   }
 }
 
@@ -21,7 +33,7 @@ export const launchRover = async (planet_id, x, y, direction) => {
     })
     return response.data
   } catch (error) {
-    throw error.response.data
+    throw getErrorMessage(error)
   }
 }
 
@@ -30,6 +42,6 @@ export const executeCommands = async (roverId, commands) => {
     const response = await axios.post(`${API_URL}/rovers/${roverId}/commands`, { commands })
     return response.data
   } catch (error) {
-    throw error.response.data
+    throw getErrorMessage(error)
   }
 }
